82a7fbda682c7a65c5fc4f632869cfb11a603ae9
osocarolio90-ALELS <osocarolio90@gmail.com>
2026-06-22 15:46:26 +0700

    ALELS 1M Foundation initial platform import

---
+++ web-react/dist/assets/input-pDXlvAZS.js
@@ -0,0 +1,6 @@
+import{c as r,r as s,j as d,d as n}from"./index-BZbbkjo2.js";/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=r("Save",[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]]),i=s.forwardRef(({className:a,type:e,...o},t)=>d.jsx("input",{type:e,className:n("flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm outline-none transition-colors placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",a),ref:t,...o}));i.displayName="Input";export{i as I,l as S};
